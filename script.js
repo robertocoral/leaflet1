422c2a5ba1ef96856c6df100ec32e99802f18dc2
Roberto Coral <33666303+robertocoral@users.noreply.github.com>
2022-01-19 18:25:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -26,19 +26,3 @@ var polygon = L.polygon([
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 circle.bindPopup("I am a circle.");
 polygon.bindPopup("I am a polygon.");
-
-var popup = L.popup()
-    .setLatLng([51.513, -0.09])
-    .setContent("I am a standalone popup.")
-    .openOn(map);
-
-var popup = L.popup();
-
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-}
-
-map.on('click', onMapClick);
